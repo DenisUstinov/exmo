eb8c1f75cb42e2e84648145ae5d6e3d6dfaaf4e9
Denis Ustinow <ml_engineer@icloud.com>
2023-03-04 17:58:01 +0700

    Improve the code for the Client class

--- exmo/websocket/client.py
+++ exmo/websocket/client.py
@@ -10,31 +10,39 @@ import websockets
 from typing import Dict, Any, Callable, Coroutine
 
 
+RECONNECT_DELAY = 5
+MAX_RECONNECT_DELAY = 10
+
+
 class Client:
     def __init__(self, response_handler: Callable[[str], Coroutine]) -> None:
         self.response_handler = response_handler
         self.logger = logging.getLogger(__name__)
 
     async def listen(self, data: Dict[str, Any]) -> None:
-        reconnect_delay = 5
+        reconnect_delay = RECONNECT_DELAY
         while True:
             try:
-                async with websockets.connect(data['url']) as websocket:
-                    for message in data.get('init_messages', []):
-                        await websocket.send(message)
-
-                    while True:
-                        response = await websocket.recv()
-                        await self.response_handler(response)
-            except websockets.exceptions.ConnectionClosed as e:
-                error_msg = f"Connection closed: {e}. Reconnecting in {reconnect_delay} seconds..."
+                await self._listen_once(data)
+            except websockets.exceptions.WebSocketException as e:
+                error_msg = f"WebSocket error: {e}. Reconnecting in {reconnect_delay} seconds..."
                 self.logger.error(error_msg, exc_info=True)
                 await asyncio.sleep(reconnect_delay)
-                reconnect_delay *= 2  # увеличиваем время между попытками вдвое
+                reconnect_delay = min(reconnect_delay * 2, MAX_RECONNECT_DELAY)
+
+    async def _listen_once(self, data: Dict[str, Any]) -> None:
+        async with websockets.connect(data['url']) as websocket:
+            for message in data.get('init_messages', []):
+                await websocket.send(message)
+
+            while True:
+                response = await websocket.recv()
+                await self.response_handler(response)
 
     @staticmethod
     def create_login_message(api_key: str, secret_key: str) -> str:
         nonce = str(time.time_ns())
         sign = hmac.new(secret_key.encode('utf8'), (api_key + nonce).encode('utf8'), hashlib.sha512).digest()
         sign = base64.b64encode(sign).decode('utf8')
-        return '{"id":1,"method":"login","api_key":"%s","sign":"%s","nonce":%s}' % (api_key, sign, nonce)
+        return f'{{"id":1,"method":"login","api_key":"{api_key}","sign":"{sign}","nonce":{nonce}}}'
+
