94f5270be94918e0d8f6763f10066579f7c71b2b
Denis Ustinow <ml_engineer@icloud.com>
2023-03-04 18:48:16 +0700

    Remove error logging client.py

--- exmo/websocket/client.py
+++ exmo/websocket/client.py
@@ -3,32 +3,24 @@ import time
 import base64
 import hashlib
 import hmac
-import logging
 
 import websockets
 
 from typing import Dict, Any, Callable, Coroutine
 
 
-RECONNECT_DELAY = 5
-MAX_RECONNECT_DELAY = 10
-
-
 class Client:
     def __init__(self, response_handler: Callable[[str], Coroutine]) -> None:
         self.response_handler = response_handler
-        self.logger = logging.getLogger(__name__)
 
     async def listen(self, data: Dict[str, Any]) -> None:
-        reconnect_delay = RECONNECT_DELAY
+        reconnect_delay = 1
         while True:
             try:
                 await self._listen_once(data)
             except websockets.exceptions.WebSocketException as e:
-                error_msg = f"WebSocket error: {e}. Reconnecting in {reconnect_delay} seconds..."
-                self.logger.error(error_msg, exc_info=True)
                 await asyncio.sleep(reconnect_delay)
-                reconnect_delay = min(reconnect_delay * 2, MAX_RECONNECT_DELAY)
+                reconnect_delay = min(reconnect_delay * 2, 10)
 
     async def _listen_once(self, data: Dict[str, Any]) -> None:
         async with websockets.connect(data['url']) as websocket:
