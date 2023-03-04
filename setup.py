b106e56c4c6064e8d103577d2855adaaa8061b89
Denis Ustinow <ml_engineer@icloud.com>
2023-03-04 08:12:36 +0700

    Completely changed setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,9 @@
 # setup.py
-import setuptools
+from setuptools import setup
 
-setuptools.setup(
+
+setup(
     name='exmo',
-    version='0.1',
-    author='Denis Ustinov',
-    packages=setuptools.find_packages(),
-    install_requires=['websockets'],
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
+    packages=['exmo'],
+    install_requires=['websockets']
 )
