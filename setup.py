896db46592bb339f42365a02ea5c76c851caf7dd
Denis Ustinow <ml_engineer@icloud.com>
2023-03-04 09:04:34 +0700

    Completely changed setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,29 @@
-# setup.py
 from setuptools import setup, find_packages
 
 setup(
-    name="exmo",
-    version="0.1",
+    name='exmo',
+    version='0.1',
+    author='ChatGPT and Denis Ustinov',
+    author_email='revers-06-checkup@icloud.com',
+    description='A Python package for working with Exmo API',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/DenisUstinov/exmo',
+    license='MIT',
     packages=find_packages(),
-    install_requires=['websockets']
+    install_requires=['websockets'],
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
 )
+
