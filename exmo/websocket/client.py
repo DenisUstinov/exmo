d66be45fe872b78be3c2b8b10043b2b045c2b6fa
Denis Ustinow <ml_engineer@icloud.com>
2023-03-04 13:11:28 +0700

    Add type annotations in client.py

--- exmo/websocket/client.py
+++ exmo/websocket/client.py
@@ -1,18 +1,16 @@
-# client.py
-import asyncio
 import time
 import hashlib
 import hmac
 import base64
 import websockets
-import json
+from typing import Dict, Any, Callable
 
 
 class Client:
-    def __init__(self, response_handler):
+    def __init__(self, response_handler: Callable[[str], None]) -> None:
         self.response_handler = response_handler
 
-    async def listen(self, data):
+    async def listen(self, data: Dict[str, Any]) -> None:
         async with websockets.connect(data['url']) as websocket:
             for message in data['init_messages']:
                 await websocket.send(message)
@@ -28,8 +26,9 @@ class Client:
                 await self.response_handler(response)
 
     @staticmethod
-    def create_login_message(api_key, secret_key):
+    def create_login_message(api_key: str, secret_key: str) -> str:
         nonce = str(time.time_ns())
         sign = hmac.new(secret_key.encode('utf8'), (api_key + nonce).encode('utf8'), hashlib.sha512).digest()
         sign = base64.b64encode(sign).decode('utf8')
         return '{"id":1,"method":"login","api_key":"%s","sign":"%s","nonce":%s}' % (api_key, sign, nonce)
+
