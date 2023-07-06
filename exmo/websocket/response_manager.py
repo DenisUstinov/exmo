cd6b8c3dea8630d74da687ccec83becc69870dcc
Denis Ustinow <ml_engineer@icloud.com>
2023-07-06 07:58:53 +0700

    Reformat project

--- exmo/websocket/response_manager.py
+++ exmo/websocket/response_manager.py
@@ -18,20 +18,36 @@ class ResponseManager:
         Returns:
             The result of handling the response data.
         """
-        event = data.get('event')
+        event = data.get("event")
         try:
-            if event in ['info', 'subscribed', 'unsubscribed', 'error', 'logger_in']:
-                return await getattr(self.actions, f'handle_{event}')(data)
-            elif event in ['snapshot', 'update']:
-                topic = data.get('topic')
-                topic_prefix = topic.split(':')[0]
-                handler_method = f'handle_{topic_prefix.replace("/", "_")}'
-                if hasattr(self.actions, handler_method):
-                    return await getattr(self.actions, handler_method)(data)
-                else:
-                    raise ValueError(f"Unrecognized data received: {topic}")
+            if event in ["info", "subscribed", "unsubscribed", "error", "logger_in"]:
+                handler_method = f"handle_{event}"
+                return await self._invoke_handler(handler_method, data)
+            elif event in ["snapshot", "update"]:
+                topic = data.get("topic")
+                topic_prefix = topic.split(":")[0].split("/")[1]
+                handler_method = f"handle_{topic_prefix}"
+                return await self._invoke_handler(handler_method, data)
             else:
                 raise ValueError(f"Unrecognized event received: {event}")
-        except NameError:
-            raise ValueError("Class not found")
+        except AttributeError as e:
+            raise ValueError(str(e)) from None
 
+    async def _invoke_handler(self, handler_method, data):
+        """
+        Invokes the specified handler method if it exists.
+
+        Args:
+            handler_method: The name of the handler method to invoke.
+            data: The data to be passed to the handler method.
+
+        Returns:
+            The result of invoking the handler method.
+
+        Raises:
+            AttributeError: If the handler method does not exist.
+        """
+        if hasattr(self.actions, handler_method):
+            return await getattr(self.actions, handler_method)(data)
+        else:
+            raise AttributeError(f"Handler method not found: {handler_method}")
